3dc3f35655d99b809fed2d35e1d2a30f61b7e53f
pirik3 <vlknsltk@gmail.com>
2026-05-19 23:12:47 +0300

    modified:   content_script.js

--- content_script.js
+++ content_script.js
@@ -1,4 +1,4 @@
-﻿// Sahibinden.com sayfasından veri çeken asıl script_lklklk11223344
+﻿// Sahibinden.com sayfasından veri çeken asıl script
 (() => {
   const host = (location.hostname || '').toLowerCase();
   const allowed = /(^|\.)sahibinden\.com$/.test(host) || host === 'parselsorgu.tkgm.gov.tr';
