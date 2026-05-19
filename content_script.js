26fea591323fafa0454f5a7b91b5eb4773a39ccb
pirik3 <vlknsltk@gmail.com>
2026-05-19 23:11:29 +0300

    modified:   content_script.js

--- content_script.js
+++ content_script.js
@@ -1,4 +1,4 @@
-﻿// Sahibinden.com sayfasından veri çeken asıl script_lklklk11
+﻿// Sahibinden.com sayfasından veri çeken asıl script_lklklk11223344
 (() => {
   const host = (location.hostname || '').toLowerCase();
   const allowed = /(^|\.)sahibinden\.com$/.test(host) || host === 'parselsorgu.tkgm.gov.tr';
